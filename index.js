ab2607eb0cfc6dd54af9fc3d1dbb738bba238fe4
malxxy <maliayraguen@gmail.com>
2023-02-05 18:19:47 -0800

    skeleton of generate team function, add itern function, add engineer function, and write to index function

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const Inquire = require('inquirer');
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
-const fs = require('fs');
-const generateTeamProfile = require('./lib/generateTeamProfile');
+// required modules
+const inquirer = require('inquirer'); // require inquirer from node modules
+const Employee = require('./lib/Employee'); // require the Employee class
+const Engineer = require('./lib/Engineer'); // require the Engineer class
+const Intern = require('./lib/Intern'); // require the Intern class
+const Manager = require('./lib/Manager'); // require the manager class
+const fs = require('fs'); // require fs to write files
+const generateTeamProfile = require('./lib/generateTeamProfile'); // require fucntion to generate the Team Profile
 
+// Questions upon launching app to insert team manager info
 const homeQuestions = [
     {
       type: 'input',
-      message: 'What is the Team Manager name?',
+      message: 'What is the team manager first and last name?',
       name: 'name',
     },
     {
       type: 'input',
-      message: 'What is the employee ID?',
+      message: 'What is their employee ID?',
       name: 'id',
     },
     {
       type: 'input',
-      message: 'What is the email address?',
+      message: 'What is their email address?',
       name: 'email',
     },
     {
       type: 'input',
-      message: 'What is the office number?',
+      message: 'What is their office number?',
       name: 'officeNumber',
     }
-  ]
+]
 
+// after entering team manager info, user given 3 options
 const additionalQs = [
     {
         type: 'list',
@@ -38,10 +41,11 @@ const additionalQs = [
     }  
 ]
 
+// questions when user chooses to add an engineer to the team
 const addEngineer = [
     {
         type: 'input',
-        message: 'What is their name?',
+        message: 'What is their first and last name?',
         name: 'name',
       },
       {
@@ -61,10 +65,11 @@ const addEngineer = [
       }
 ]
 
+// questions when user chooses to add an intern
 const addIntern = [
     {
         type: 'input',
-        message: 'What is their name?',
+        message: 'What is their first and last name?',
         name: 'name',
     },
     {
@@ -82,20 +87,46 @@ const addIntern = [
         message: 'What school does the intern attend?',
         name: 'school',
     }
-]
-  
-  
+] 
+
+function addAnEngineer(responses) {
+  inquirer.prompt(addEngineer).then((engineerInfo) => {
+    engineer = new Engineer(engineerInfo.name,engineerInfo.id,engineerInfo.email,engineerInfo.github);
+  });
+};
+
+function addAnIntern(responses) {
+  inquirer.prompt(addIntern).then((internInfo) => {
+    intern = new Intern(internInfo.name,internInfo.id,internInfo.email,internInfo.school);
+  });
+};
+
 // Function to write the html file
 function writeToFile(readMe, data) {
-    return fs.writeFileSync(path.join(process.cwd(),readMe),data)
-    };
-  
-// Function to initialize app 
-  function init() {
-    inquirer.prompt(questions).then((inquirerResponses) => {
-      writeToFile('myTeam.html',generateTeamProfile(inquirerResponses));
+  return fs.writeFileSync(path.join(process.cwd(),readMe),data)
+};
+
+// function to close app and generate HTML file, and open HTML file
+function finishApp(responses) {
+  writeToFile('myTeam.html',generateTeamProfile(responses));
+};
+
+function generateTeam () {
+  inquirer.prompt(homeQuestions).then((responses) => {
+    teamManager = new Manager(responses.name,responses.id,responses.email,responses.officeNumber)
+    .then((nextSteps) => {
+      inquirer.prompt(additionalQs).then((nextSteps) => {
+        if  (nextSteps === 'Add an engineer') {
+          addAnEngineer();
+        } else if (nextSteps === 'Add an intern') {
+          addAnIntern();
+        } else if (nextSteps === `Finish building the team`) {
+          finishApp();
+        }
+      });
     });
-    }
-  
-  // Call initialization
-  init();
+  });
+};
+
+// function to initialize app 
+generateTeam();
