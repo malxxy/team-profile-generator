6616c782283342c5d82376e52a15df9c9be0cc1c
malxxy <maliayraguen@gmail.com>
2023-02-05 16:32:09 -0800

    tests and lib skeleton code, index.html template

---
+++ tests/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+    it('should return name', () => {
+      const name = 'NameTest'
+
+      const expectedName = new Employee(name);
+      expect(expectedName.name).toEqual('NameTest');
+    });
+  
+    it('should return id', () => {
+        const id = '00'
+
+        const expectedId = new Employee(id);
+        expect(expectedId.id).toEqual('00');
+    });
+  
+    it('should return email', () => {
+        const email = 'example@example.com'
+
+        const expectedEmail = new Employee(email);
+        expect(expectedEmail.email).toEqual('example@example.com');
+    });
+
+  it('should return role', () => {
+    const role = 'Intern'
+
+    const expectedRole = new Employee(role);
+    expect(expectedRole.role).toEqual('Intern');
+  });
+});
---
+++ tests/Engineer.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+describe('getEmployee', () => {
+    it('should return name', () => {
+      // code
+    });
+  
+    it('should return id', () => {
+    // code
+    });
+  
+    it('should return email', () => {
+    // code
+    });
+
+  it('should return role', () => {
+    // code
+  });
+});
